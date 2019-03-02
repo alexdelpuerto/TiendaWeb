e14cc81bb96fadb8a598395896d8a3709541fcf6
Alex <alejandro.delpuerto2@gmail.com>
2019-03-02 12:25:04 +0100

    Added PedidosAPI Controller and FacturasAPI Controller issue #5

---
+++ TiendaWeb/Controllers/API/FacturasAPIController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using TiendaWeb.Models;
+
+namespace TiendaWeb.Controllers.API
+{
+    public class FacturasAPIController : ApiController
+    {
+        private ModeloTiendaWebContainer db = new ModeloTiendaWebContainer();
+
+        // GET: api/FacturasAPI
+        public IQueryable<Factura> GetFacturas()
+        {
+            return db.Facturas;
+        }
+
+        // GET: api/FacturasAPI/5
+        [ResponseType(typeof(Factura))]
+        public IHttpActionResult GetFactura(int id)
+        {
+            Factura factura = db.Facturas.Find(id);
+            if (factura == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(factura);
+        }
+
+        // PUT: api/FacturasAPI/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutFactura(int id, Factura factura)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != factura.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(factura).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!FacturaExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/FacturasAPI
+        [ResponseType(typeof(Factura))]
+        public IHttpActionResult PostFactura(Factura factura)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Facturas.Add(factura);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = factura.Id }, factura);
+        }
+
+        // DELETE: api/FacturasAPI/5
+        [ResponseType(typeof(Factura))]
+        public IHttpActionResult DeleteFactura(int id)
+        {
+            Factura factura = db.Facturas.Find(id);
+            if (factura == null)
+            {
+                return NotFound();
+            }
+
+            db.Facturas.Remove(factura);
+            db.SaveChanges();
+
+            return Ok(factura);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool FacturaExists(int id)
+        {
+            return db.Facturas.Count(e => e.Id == id) > 0;
+        }
+    }
+}
